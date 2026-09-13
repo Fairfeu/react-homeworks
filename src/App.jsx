b1143142cc0243e625330c9185a508c6701d5f3f
Oleg <fnytseu@gmail.com>
2026-09-13 20:25:18 +0300

    rebuilt files logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
-import "./App.css";
-
-function Header() {
-  return <header>Header</header>;
-}
-
-function MainContent() {
-  return (
-    <main>
-      <Section />
-    </main>
-  );
-}
-
-function Section() {
-  return (
-    <>
-      <h1>Head</h1>
-      <p>section txts</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel
-        saepe dolores officiis eaque! Hic, repellat repellendus ea voluptatibus
-        molestiae nihil, tempora aliquid debitis quis quibusdam placeat,
-        voluptate ipsam dolore.
-      </p>
-    </>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <p>2026</p>
-    </footer>
-  );
-}
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Section, MainContent } from './components/Content'
 
 function App() {
   return (
@@ -44,6 +12,6 @@ function App() {
       </MainContent>
       <Footer />
     </>
-  );
+  )
 }
-export default App;
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,22 @@
+export function MainContent() {
+  return (
+    <main>
+      <Section />
+    </main>
+  )
+}
+
+export function Section() {
+  return (
+    <>
+      <h1>Head</h1>
+      <p>section txts</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel
+        saepe dolores officiis eaque! Hic, repellat repellendus ea voluptatibus
+        molestiae nihil, tempora aliquid debitis quis quibusdam placeat,
+        voluptate ipsam dolore.
+      </p>
+    </>
+  )
+}
